ffb9769ec0da5c3eda786ec52774127b063e75f1
MuhammadUsmanGM <muhammadusman75425965etc@gmail.com>
2026-09-07 19:40:19 +0500

    chore: update LinkedIn profile URL in MainLayout social links

--- src/components/MainLayout.vue
+++ src/components/MainLayout.vue
@@ -77,7 +77,7 @@ const handlePortfolioClick = (e: MouseEvent) => {
                 <Github :size="18" />
                 <span>GITHUB</span>
               </a>
-              <a href="https://www.linkedin.com/in/muhammad-usman-ai-dev" target="_blank" class="social-link">
+              <a href="https://www.linkedin.com/in/muhammadusmangm" target="_blank" class="social-link">
                 <Linkedin :size="18" />
                 <span>LINKEDIN</span>
               </a>
